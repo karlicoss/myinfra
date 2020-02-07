332bdae697fb19203f01e50d32a0fa2759532e11
Dima Gerasimov <karlicoss@gmail.com>
2020-02-07 10:01:48 +0000

    patch all edges

--- postprocess.py
+++ postprocess.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python3
 from pathlib import Path
-from lxml import etree as ET
+from lxml import etree as ET # type: ignore
 
+NS = '{http://www.w3.org/2000/svg}'
 def ns(s):
-    return '{http://www.w3.org/2000/svg}' + s
+    return NS + s
 
 
-def run(inp: Path) -> ET.ElementTree:
-    root = ET.parse(str(inp))
-    # TODO meh
-    elems = root.findall('.//{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}text')
-    [ce] = [e for e in elems if e.text == 'Calendar']
-    c = ce.getparent()
-    cp = c.find(ns('path'))
-    cp.attrib['id'] = 'hello'
+def fix_edge(e):
+    eid = e.attrib['id']
+    lid = 'label_' + eid
 
-    ct = ce
+    p = e.find(ns('path'))
 
-    del ct.attrib['x']
-    del ct.attrib['y']
-    del ct.attrib['text-anchor']
-    label = ct.text
-    ct.text = None
-    tp = ET.SubElement(ct, 'textPath')
-    tp.attrib['href'] = f'#hello'
+    if p is None:
+        return
+
+    t = e.find(ns('text'))
+    if t is None:
+        return
+
+    p.attrib['id'] = lid
+
+    del t.attrib['x']
+    del t.attrib['y']
+    del t.attrib['text-anchor']
+    label = t.text
+    t.text = None
+    tp = ET.SubElement(t, 'textPath')
+    tp.attrib['href'] = '#' + lid
     tp.attrib['startOffset'] = '10%'
     tp.attrib['side'] = 'right'
     tp.text = label
     # TODO url?
 
+
+def run(inp: Path) -> ET.ElementTree:
+    root = ET.parse(str(inp))
+    edges = root.findall(f'.//{NS}g[@class="edge"]')
+
+    for e in edges:
+        fix_edge(e)
+
     return root
+
 
 def main():
     res = run(Path('infra.svg'))
